1afb16be022ce509a6f6c61ea2cbde85e00ebcb2
高志刚 <gaozhigang@gaozhigangdeMacBook-Air.local>
2015-06-16 00:18:32 +0800

    Initial Commit

---
+++ Dota Helper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dota Helper
+//
+//  Created by 高志刚 on 15/6/16.
+//  Copyright (c) 2015年 高志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dota Helper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dota Helper
+//
+//  Created by 高志刚 on 15/6/16.
+//  Copyright (c) 2015年 高志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
